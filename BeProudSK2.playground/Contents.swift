af9d0aef1cfd08bd0cdeac7ec27a9e619fbea9ad
Turma Manhã <turma.manha@brufpe-09.local>
2018-03-28 13:28:47 -0300

    Fix movement and obstacles

--- BeProudSK2.playground/Contents.swift
+++ BeProudSK2.playground/Contents.swift
@@ -3,12 +3,14 @@ import SpriteKit
 
 struct PhysicsCategory {
     static let None: UInt32 = 0
-    static let Person: UInt32 = 0b1      // Person
-    static let Obstacles: UInt32 = 0b10  // Obstacles
+    static let Person: UInt32 = 0b1       // Person
+    static let Obstacles: UInt32 = 0b10   // Obstacles
+    static let Acceptance: UInt32 = 0b101 // Acceptance
 }
 
 public class RedScene: SKScene, SKPhysicsContactDelegate {
     var personNode = SKShapeNode(circleOfRadius: 45)
+    var acceptanceNode = SKShapeNode(rectOf: CGSize(width: 20, height: 20), cornerRadius: 3)
     
     override public func didMove(to view: SKView) {
         // Setting up physics word
@@ -17,6 +19,7 @@ public class RedScene: SKScene, SKPhysicsContactDelegate {
         
         self.createPerson()
         self.createObstacles()
+        self.createAceptance()
         self.physicsBody = SKPhysicsBody(edgeLoopFrom: self.frame)
     }
     
@@ -26,89 +29,118 @@ public class RedScene: SKScene, SKPhysicsContactDelegate {
         
         person.isAntialiased = false
         person.lineWidth = 0
-        person.fillColor = UIColor.white
+        person.fillColor = UIColor.black
         person.position = CGPoint(x: 50, y: 400)
+
+        
         person.physicsBody = body
         person.physicsBody?.affectedByGravity = false
-        personNode.physicsBody?.mass = 0
+        person.physicsBody?.mass = 9999
+        person.physicsBody?.restitution = 20
+        person.physicsBody?.categoryBitMask = PhysicsCategory.Person
+        person.physicsBody?.contactTestBitMask = PhysicsCategory.Acceptance
+        person.physicsBody?.collisionBitMask = PhysicsCategory.Obstacles
         
         self.addChild(person)
     }
     
     func createObstacles() {
-        var obstacles = [
-            CGRect(x: 0, y: 0, width: 8, height: 240), // 1
-            CGRect(x: 0, y: 0, width: 30, height: 8), // 2
+        let sizes = [
+            (w: 10, h: 240), // 1
+            (w: 250, h: 10), // 2
+            (w: 10, h: 240), // 3
+            (w: 500, h: 10), // 4
+            (w: 10, h: 445), // 5
+            (w: 110, h: 10), // 6
+            (w: 10, h: 110), // 7
         ]
         
-        var physicsBodies = [
-            CGSize(width: 8, height: 220), // 1
-            CGSize(width: 30, height: 8), // 2
+        let positions = [
+            CGPoint(x: 140, y: 300), // 1
+            CGPoint(x: 0, y: 200),   // 2
+            CGPoint(x: 250, y: 200), // 3
+            CGPoint(x: 125, y: 95),  // 4
+            CGPoint(x: 440, y: 95),  // 5
+            CGPoint(x: 640, y: 250), // 6
+            CGPoint(x: 640, y: 250), // 7
         ]
-        
-        var positions = [
-            CGPoint(x: 140, y: 260), // 1
-            CGPoint(x: 110, y: 300), // 2
-        ]
-        
-        var wall: SKShapeNode
-        for n in 0...obstacles.count - 1 {
-            wall = SKShapeNode(rect: obstacles[n])
-            wall.fillColor = UIColor.white
-            wall.position = positions[n]
-            
-            wall.physicsBody = SKPhysicsBody(rectangleOf: physicsBodies[n] )
-            wall.physicsBody?.affectedByGravity = false
-            wall.physicsBody?.pinned = true
-            wall.physicsBody?.allowsRotation = false
-            wall.physicsBody?.categoryBitMask = PhysicsCategory.Obstacles
-            wall.physicsBody?.contactTestBitMask = PhysicsCategory.Person
-            wall.physicsBody?.collisionBitMask = PhysicsCategory.Person
-            
-            self.addChild(wall)
+
+        var obstacles: SKShapeNode
+        for (size, position) in zip(sizes, positions) {
+            obstacles = SKShapeNode(rect: CGRect(x: 0, y: 0, width: size.w, height: size.h))
+            obstacles.fillColor = UIColor.black
+            obstacles.position = position
+            obstacles.lineWidth = 0
+
+            obstacles.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: size.w, height: size.h), center: CGPoint(x: size.w/2, y: size.h/2))
+            obstacles.physicsBody?.affectedByGravity = false
+            obstacles.physicsBody?.pinned = true
+            obstacles.physicsBody?.allowsRotation = false
+            obstacles.physicsBody?.isDynamic = false
+            obstacles.physicsBody?.categoryBitMask = PhysicsCategory.Obstacles
+            obstacles.physicsBody?.contactTestBitMask = PhysicsCategory.Person
+            obstacles.physicsBody?.collisionBitMask = PhysicsCategory.Person
+            obstacles.physicsBody?.mass = 999
+
+            self.addChild(obstacles)
         }
     }
     
-    var isTouching = false
-//    var haveCollision = false
-    var positionOfTouch = CGPoint(x: 0 , y: 0)
+    func createAceptance() {
+        let acceptance = self.acceptanceNode
+        acceptance.position = CGPoint(x: 698, y: 305)
+        acceptance.fillColor = UIColor.red
+        acceptance.strokeColor = UIColor.red
+        acceptance.zRotation = 40
+        
+        acceptance.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 20, height: 20))
+        acceptance.physicsBody?.affectedByGravity = false
+        acceptance.physicsBody?.pinned = true
+        acceptance.physicsBody?.allowsRotation = false
+        acceptance.physicsBody?.isDynamic = false
+        acceptance.physicsBody?.categoryBitMask = PhysicsCategory.Acceptance
+        acceptance.physicsBody?.contactTestBitMask = PhysicsCategory.Person
+        acceptance.physicsBody?.collisionBitMask = PhysicsCategory.Person
+        acceptance.physicsBody?.mass = 999
+        
+        self.addChild(acceptance)
+    }
     
     public func didBegin(_ contact: SKPhysicsContact) {
-        print("have collision")
+        self.personNode.removeAction(forKey: "moving")
+        if contact.bodyA.categoryBitMask == PhysicsCategory.Acceptance ||
+            contact.bodyB.categoryBitMask == PhysicsCategory.Acceptance {
+            
+            let acceptYourself = SKAction.fadeOut(withDuration: 1)
+            self.acceptanceNode.run(acceptYourself)
+        }
     }
     
     override public func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.isTouching = true
-        for t in touches { self.positionOfTouch = t.location(in: self) }
+        for t in touches { self.move(to: t.location(in: self)) }
     }
     
-    override public func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.isTouching = false
-//        self.haveCollision = false
-    }
-    
-    override public func update(_ currentTime: TimeInterval) {
-        if self.isTouching {
-            let pos = self.positionOfTouch
-            
-            if pos.x < 250 {
-                self.personNode.position = CGPoint(x: self.personNode.position.x - 1, y: self.personNode.position.y)
-            } else if pos.x > 500 {
-               self.personNode.position = CGPoint(x: self.personNode.position.x + 1, y: self.personNode.position.y)
-            } else if pos.y >= 250 {
-                self.personNode.position = CGPoint(x: self.personNode.position.x, y: self.personNode.position.y + 1)
-            } else {
-               self.personNode.position = CGPoint(x: self.personNode.position.x, y: self.personNode.position.y - 1)
-            }
+    func move(to pos: CGPoint) {
+        if pos.x < 250 {
+            let moveAction = SKAction.moveTo(x: 0, duration: 3)
+            self.personNode.run(moveAction, withKey: "moving")
+        } else if pos.x > 500 {
+            let moveAction = SKAction.moveTo(x: 750, duration: 3)
+            self.personNode.run(moveAction, withKey: "moving")
+        } else if pos.y >= 270 {
+            let moveAction = SKAction.moveTo(y: 500, duration: 3)
+            self.personNode.run(moveAction, withKey: "moving")
+        } else if pos.y < 250 {
+            let moveAction = SKAction.moveTo(y: 0, duration: 3)
+            self.personNode.run(moveAction, withKey: "moving")
         }
     }
 }
 
 
-let sceneView = SKView(frame: CGRect(x:0 , y:0, width: 750, height: 500))
-let startScene = RedScene(size: CGSize(width: 750, height: 500))
+let sceneView = SKView(frame: CGRect(x:0 , y:0, width: 750, height: 540))
+let startScene = RedScene(size: CGSize(width: 750, height: 540))
 startScene.backgroundColor = UIColor.gray
-
 startScene.scaleMode = .aspectFill
 sceneView.presentScene(startScene)
 PlaygroundSupport.PlaygroundPage.current.liveView = sceneView
